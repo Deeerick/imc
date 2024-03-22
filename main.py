d05d11aa0c5c14965ede59a08ad18798d554734d
Deeerick <dspsoares@outlook.com>
2024-03-22 17:19:41 -0300

    First Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Calculadora de IMC")
+
+peso = float(input("Qual o seu peso em KG?"))
+altura = float(input("Qual a sua altura em melhor"))
+
+imc = peso / (altura ** 2)
+
+print("Seu IMC é de: ")
