b5bf3ae79ad5d56db3701908b11e377f9c2f9338
Deeerick <dspsoares@outlook.com>
2024-03-23 11:36:54 -0300

    Ajustes no Código

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-print("Calculadora de IMC")
+print('=' * 10 + '\033[91m 🚨 Inicializando a Calculadora de IMC 🚨 \033[0m' + '=' * 10)
 
-peso = float(input("Qual o seu peso em KG?"))
-altura = float(input("Qual a sua altura em melhor"))
+peso = float(input("\033[91m > Qual o seu peso em KG?  \033[0m"))
+altura_cm = float(input("\033[91m > Qual a sua altura em centímetros?  \033[0m"))
 
-imc = peso / (altura ** 2)
+altura_m = altura_cm / 100
+imc = peso / (altura_m ** 2)
 
-print("Seu IMC é de: ")
+print("\033[91m > Seu IMC é de {:.1f} \033[0m".format(imc))
+
+if imc < 18.5:
+    print("\033[91m > Classificação = MAGREZA \033[0m")
+elif 18.5 <= imc < 24.9:
+    print("\033[91m > Classificação = NORMAL \033[0m")
+elif 25.0 <= imc < 29.9:
+    print("\033[91m > Classificação = SOBREPESO \033[0m")
+elif 30.0 <= imc < 39.9:
+    print("\033[91m > Classificação = OBESIDADE \033[0m")
+else:
+    print("\033[91m > Classificação = OBESIDADE GRAVE \033[0m")
